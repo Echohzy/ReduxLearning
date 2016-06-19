c8756b4fb102ccd84430c4f1b4ed4edf371ee490
Echo <15372418416@163.com>
2016-06-19 16:05:08 +0800

    add assets

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 var koa = require("koa");
+var serve = require("koa-static");
 var main = require("./routes/main.js");
 var app = koa();
+
+app.use(serve("./assets"));
 
 app.use(main.get);
 app.listen(8080);
